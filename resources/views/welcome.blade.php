d7cc6a0f9f69fc4cd5e61626528a3a5fea04f6aa
bad4iz <info@bad4iz.ru>
2018-03-10 22:51:43 +0400

    test deploy from travis in heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    dsdsdsd
                 </div>
 
                 <div class="links">
